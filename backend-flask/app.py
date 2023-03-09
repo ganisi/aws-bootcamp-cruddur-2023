ce0d06c9d27dc203feca8db0ea3c287dae79a9ee
ganisi <ganisi@abv.bg>
2023-03-09 19:26:52 +0000

    token working

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -163,15 +163,13 @@ def data_home():
   access_token = extract_access_token(request.headers)
   try:
     claims = cognito_jwt_token.verify(access_token)
-    # authenicatied request
-
+    #authenicatied request
     app.logger.debug("authenicated")
     app.logger.debug(claims)
     app.logger.debug(claims['username'])
     data = HomeActivities.run(cognito_user_id=claims['username'])
   except TokenVerifyError as e:
-    # unauthenicatied request
-
+    #unauthenicatied request
     app.logger.debug(e)
     app.logger.debug("unauthenicated")
     data = HomeActivities.run()
