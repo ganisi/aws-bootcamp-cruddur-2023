26d4ff8eae4dd3b5a3e7f05d9f09d1398e8930af
ganisi <ganisi@abv.bg>
2023-03-28 17:04:42 +0000

    lando message initiated

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,7 +2,8 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Gabriel Simeonov','ganisi@abv.bg' , 'gabrielsimeonov' ,'Your_cognito_user_id'),
-  ('Spas Kyosev','4em6iro@gmail.com' , 'spaskyosev' ,'Your_cognito_user_id');
+  ('Spas Kyosev','4em6iro@gmail.com' , 'spaskyosev' ,'Your_cognito_user_id'),
+  ('Londo Mollari','lmollari@centari.com' , 'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
