e0f99df43f87c953453380d2eadb18d32b2be7ce
ganisi <ganisi@abv.bg>
2023-03-28 14:04:52 +0000

    conversations appear/getting cognito users

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -143,7 +143,7 @@ def data_message_groups():
 
 @app.route("/api/messages/@<string:handle>", methods=['GET'])
 def data_messages(handle):
-  user_sender_handle = 'andrewbrown'
+  user_sender_handle = 'gabrielsimeonov'
   user_receiver_handle = request.args.get('user_reciever_handle')
 
   model = Messages.run(user_sender_handle=user_sender_handle, user_receiver_handle=user_receiver_handle)
@@ -156,7 +156,7 @@ def data_messages(handle):
 @app.route("/api/messages", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_create_message():
-  user_sender_handle = 'andrewbrown'
+  user_sender_handle = 'gabrielsimeonov'
   user_receiver_handle = request.json['user_receiver_handle']
   message = request.json['message']
 
@@ -229,7 +229,7 @@ def data_show_activity(activity_uuid):
 @app.route("/api/activities/<string:activity_uuid>/reply", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities_reply(activity_uuid):
-  user_handle  = 'andrewbrown'
+  user_handle  = 'gabrielsimeonov'
   message = request.json['message']
   model = CreateReply.run(message, user_handle, activity_uuid)
   if model['errors'] is not None:
--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
-
+-- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK');
+  ('Gabriel Simeonov','ganisi@abv.bg' , 'gabrielsimeonov' ,'Your_cognito_user_id'),
+  ('Spas Kyosev','4em6iro@gmail.com' , 'spaskyosev' ,'Your_cognito_user_id');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
