0e090a05e069cabaadc9b7041c2b509024109880
ganisi <ganisi@abv.bg>
2023-04-20 18:54:44 +0000

    fix a few things and the times

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,12 +1,14 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Spas Kyosev','4em6iro@gmail.com' , 'spaskyosev' ,'0feb303c-12be-46b5-b5a4-96b734a3ae5a');
+  ('Gabriel Simeonov','ganisi@abv.bg' , 'gabrielsimeonov' ,'7d5ce73d-4992-4a2b-9959-42d518cdd7d2'),
+  ('Spas Kyosev','4em6iro@gmail.com' , 'spaskyosev' ,'0feb303c-12be-46b5-b5a4-96b734a3ae5a'),
+  ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'gabrielsimeonov' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
