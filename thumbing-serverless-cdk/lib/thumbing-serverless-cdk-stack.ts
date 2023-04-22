3b8e6e77bafd2db904fc7a83e021d80f9d0f685b
ganisi <ganisi@abv.bg>
2023-04-22 16:30:59 +0000

    lambdas and serverless bin

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 const dotenv = require('dotenv');
@@ -27,7 +28,10 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
       
     
     const lambda = this.createLambda(folderInput,folderOutput,functionPath,bucketName)
-    const bukcet = this.createBucket(bucketName);
+    const bucket = this.createBucket(bucketName);
+
+    this.createS3NotifyToLambda(folderInput,lambda,bucket)
+
   }
     
   createBucket(bucketName: string): s3.IBucket {
@@ -54,6 +58,14 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
       }
     });
     return lambdaFunction;
+  }
+
+  createS3NotifyToLambda(prefix: string, lambda: lambda.IFunction, bucket: s3.IBucket): void {
+    const destination = new s3n.LambdaDestination(lambda);
+      bucket.addEventNotification(s3.EventType.OBJECT_CREATED_PUT,
+      destination,
+      {prefix: prefix}
+    )
   }
 
 }
