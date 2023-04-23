21b951cb7eb6c9ea5d7797a215c7e6eb53066ee5
ganisi <ganisi@abv.bg>
2023-04-23 17:05:17 +0000

    lambdas and bash scripts

--- frontend-react-js/src/components/ProfileAvatar.js
+++ frontend-react-js/src/components/ProfileAvatar.js
@@ -1,7 +1,7 @@
 import './ProfileAvatar.css';
 
 export default function ProfileAvatar(props) {
-  const backgroundImage = `url("https://assets.beici-demo.xyz/avatars/${props.id}.jpg")`;
+  const backgroundImage = `url("https://assets.cruddurapp.link/avatars/${props.id}.jpg")`;
   const styles = {
     backgroundImage: backgroundImage,
     backgroundSize: 'cover',
